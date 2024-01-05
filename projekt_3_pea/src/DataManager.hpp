149faa8a3e914429f09d125d180f1827b9ca8108
Jocho14 <jochotyler1@gmail.com>
2024-01-05 16:06:35 +0100

    2 metody krzyzowania

---
+++ projekt_3_pea/src/DataManager.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class DataManager 
+{
+public:
+	// metoda do odczytywania wyniku (cyklu) z pliku
+	static std::vector<int> readFile(const std::string& fileName);
+
+	// metod do zapisywania wyniku (cyklu) do pliku
+	static void saveFile(std::vector<int> cycle, std::string fileName);
+
+	// metoda do wyœwietlania cyklu w konsoli
+	static void displayVector(std::vector<int> cycle);
+
+	// metoda do dodania speratorów w pliku tekstowym
+	static void saveSeparators(std::string fileName);
+
+	// metoda do zapisywania danych do sprawozdania
+	static void saveFileTest(int minCycle, long long time, double a, double tk, const std::string& fileName);
+};
